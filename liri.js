e590ab3c6ed8a9ceec2cba7ed94b68af926dc6cc
annalisa leote <amleote@gmail.com>
2017-09-27 18:38:08 -0400

    added tweet created at

--- liri.js
+++ liri.js
@@ -10,21 +10,10 @@ var Spotify = require('node-spotify-api');
 var fs = require("fs");
 
 
-// // this reads the command line
-// var commandArrgs = process.argv;
-// //this is the command liri will recieve 
-// var liriCommand = commandArrgs[2];
-// //
-// var liriArrgs = "";
-// for (var i =3; i <commandArrgs.length; i++){
-// 	liriArrgs += commandArrgs[i] + " ";
-// }
 
-var T = new twitter(keys.twitterKeys);
+var Tweety = new twitter(keys.twitterKeys);
 var params = {
 	count: 20
-
-
 }
 
 
@@ -32,7 +21,7 @@ var liriArrgs = process.argv[2];
 
 
 function myTweets ( ) {
-T.get('statuses/user_timeline', params, gotData);
+Tweety.get('statuses/user_timeline', params, gotData);
 }
 function gotData(err, data, response){
    fs.writeFile("log.txt", "tweets", function(err) {
@@ -43,18 +32,19 @@ function gotData(err, data, response){
 
 	// console.log(data);
 	for (var i = 0; i < data.length; i++) {
-		// data[i].text
+		"Tweet:" + data[i].text + "Created On:" + data[i].created_at
 
 
-		// console.log(data[i].text);
-// var tweetsArray = [];
+		console.log(data[i].text);
+		console.log(data[i].created_at);
+var tweetsArray = [];
 var tweetieBird = data[i].text;
-// tweetsArray.push(tweetieBird);
+tweetsArray.push(tweetieBird);
 getTweets(tweetieBird);
 		
 	}
-	// console.log(tweetsArray);
-	// getTweets();
+	console.log(tweetsArray);
+	getTweets();
 
 }
 
@@ -71,24 +61,7 @@ if (liriArrgs === "my-tweets"){
 
 
 
-// switch(liriArrgs){
 
-// 	case 'my-tweets':
-// 	myTweets();
-// 	break;
-
-// }
-
-//what louis sent me 
-// var params = {
-//         screen_name: 'botus'
-//     };
-//     keys.client.get('statuses/user_timeline', params, function(error, tweets, response) {
-//         if (error) {
-//             console.log("you got a error in here");
-//         }
-//need to display last 20 tweets 
-// \n\n makes the data display vertically 
 function getTweets(tweets) {
 	fs.appendFile("log.txt",tweets+"\n\n", function(err) {
 		if (err){
@@ -100,13 +73,6 @@ function getTweets(tweets) {
 
 }
 
-// this is what I entered from npm's website 
-// var client = new Twitter(twitterKeys);
-// var params = {screen_name:"amleote", count:20};
-
-// client.get("statuses/user_timeline", params, function(error, tweets, response) {
-// 	if (error) {
-// 		console.log(err);
 
 	
 
